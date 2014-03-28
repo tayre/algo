2459b4246a619780a2eaddbb37f267f4d842f7c4
tayre <ayre.tom@gmail.com>
2014-03-28 14:39:32 -0400

    pretty print

--- bst.js
+++ bst.js
@@ -1,66 +1,44 @@
-if (typeof console === 'undefined')
-{
-    console =
-    {
-        log : function (str)
-        {
-            print(str);
-        }
-
-    };
-}
-
-function Node(data, left, right)
-{
+function Node(data, left, right) {
     this.data = data;
     this.left = left;
     this.right = right;
     this.show = show;
 }
 
-function show()
-{
+function show() {
     return this.data;
 }
 
-function BST()
-{
+function BST() {
     this.root = null;
     this.insert = insert;
     this.inOrder = inOrder;
     this.preOrder = preOrder;
 }
 
-function insert(data)
-{
+function insert(data) {
     var node = new Node(data, null, null);
 
     //bst doesn't yet have a root element set
-    if (this.root === null)
-    {
+    if (this.root === null) {
         this.root = node;
     }
-    else
-    {
+    
+    else {
         var currNode = this.root;
-        while (true)
-        {
-            if (data < currNode.data && currNode.left === null)
-            {
+        while (true) {
+            if (data < currNode.data && currNode.left === null) {
                 currNode.left = node;
                 break;
             }
-            else if (data > currNode.data && currNode.right === null)
-            {
+            else if (data > currNode.data && currNode.right === null) {
                 currNode.right = node;
                 break;
             }
-            else if (data < currNode.data && currNode.left !== null)
-            {
+            else if (data < currNode.data && currNode.left !== null) {
                 currNode = currNode.left;
             }
-            else if (data > currNode.data && currNode.right !== null)
-            {
+            else if (data > currNode.data && currNode.right !== null) {
                 currNode = currNode.right;
             }
         }
@@ -68,10 +46,8 @@ function insert(data)
 }
 
 //in order traverse of BST rooted at node
-function inOrder(node)
-{
-    if (node !== null)
-    {
+function inOrder(node) {
+    if (node !== null) {
         inOrder(node.left);
         console.log(node.show() + "");
         inOrder(node.right);
@@ -79,34 +55,26 @@ function inOrder(node)
 }
 
 // pre order traverse of BST rooted at node
-function preOrder(node)
-{
-    if (node !== null)
-    {
+function preOrder(node) {
+    if (node !== null) {
         preOrder(node.left);
         preOrder(node.right);
     }
 }
 
 // return minimal node in BST rooted at node
-function findMin(node)
-{
-
+function findMin(node) {
     var currNode = node;
-    while (currNode.left != null)
-    {
-
+    
+    while (currNode.left != null) {
         currNode = currNode.left;
-
     }
 
     return currNode.data;
-
 }
 
 // entry
-(function main()
-{
+(function main() {
     var nums = new BST();
     nums.insert(23);
     nums.insert(45);
